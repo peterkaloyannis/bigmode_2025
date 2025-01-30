cc7a1a55b32302351f770eca70c9908b10f75ac2
HenriLamarre <henrilamarre@hotmail.com>
2025-01-29 21:54:40 -0500

    Layout for the overdrives

--- Assets/Scripts/Arms/FightAudioManager.cs
+++ Assets/Scripts/Arms/FightAudioManager.cs
@@ -4,34 +4,41 @@ public class FightAudioManager : MonoBehaviour
 {
     private AudioClip[] Loops;
     private AudioSource[] MusicSources;
+    private double goalTime = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Loops = Resources.LoadAll<AudioClip>("Sounds/Music/FightScene");
-        MusicSources = new AudioSource[Loops.Length];
+        MusicSources = new AudioSource[2*Loops.Length];
         foreach (AudioClip loop in Loops){
             GameObject audioObject = new GameObject(loop.name);
             audioObject.transform.SetParent(this.transform);
             AudioSource audioSource = audioObject.AddComponent<AudioSource>();
-            MusicSources[System.Array.IndexOf(Loops, loop)] = audioSource;
+            MusicSources[2*System.Array.IndexOf(Loops, loop)] = audioSource;
+            AudioSource audioSource2 = audioObject.AddComponent<AudioSource>();
+            MusicSources[2*System.Array.IndexOf(Loops, loop)+1] = audioSource2;
             audioSource.clip = loop;
+
+            MusicSources[2*System.Array.IndexOf(Loops, loop)].PlayScheduled(AudioSettings.dspTime + 1);
+            goalTime = AudioSettings.dspTime + 1 + (double)(loop.samples) / loop.frequency;
+            MusicSources[2*System.Array.IndexOf(Loops, loop)].volume = 0f;
+            MusicSources[2*System.Array.IndexOf(Loops, loop)+1].volume = 0f;
         }
 
-        foreach (AudioSource Music in MusicSources){
-            Music.Play();
-            Music.loop = true;
-            Music.volume = 0f;
-        }
+        SelectTrack(0);
     }
 
 
     void SelectTrack(int track){
-        foreach (AudioSource Music in MusicSources){
-            Music.volume = 0f;
+        for (int i = 0; i < Loops.Length; i++){
+            MusicSources[2*i].volume = 0f;
+            MusicSources[2*i+1].volume = 0f;
         }
-        MusicSources[track].volume = 1f;
+        MusicSources[2*track].volume = 1f;
+        MusicSources[2*track+1].volume = 1f;
     }
 
+    int osc;
     // Update is called once per frame
     void Update()
     {
@@ -45,6 +52,21 @@ public class FightAudioManager : MonoBehaviour
             SelectTrack(3);
         } else if (Input.GetKeyDown(KeyCode.Alpha5)){
             SelectTrack(4);
+        } 
+
+        if (AudioSettings.dspTime > goalTime - 1)
+        {
+            if (MusicSources[0].isPlaying){
+                osc = 1;
+            } else {
+                osc = 0;
+            }
+
+            foreach (AudioClip loop in Loops){
+                MusicSources[2*System.Array.IndexOf(Loops, loop)+osc].clip = loop;
+                MusicSources[2*System.Array.IndexOf(Loops, loop)+osc].PlayScheduled(goalTime);
+            }
+            goalTime = goalTime + (double)(Loops[0].samples) / Loops[0].frequency;
         }
     }
 }
